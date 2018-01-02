d0aedd72c8ed6eb8e8780790a7fa3836517865f7
xshrxf11 <ashrafjeff@gmail.com>
2018-01-03 00:57:47 +0800

    added Datatables & FontAwesome

---
+++ resources/views/superadmin/mosque.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+        <div class="row">
+            @component('components.menu')
+            @endcomponent
+
+            <div class="col-md-9">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Super Admin Dashboard</div>
+        
+                        <div class="panel-body">
+                                <div class="table-responsive">
+                                        <table class="table table-hover table-bordered table-striped datatable" style="width:100%">
+                                                <thead>
+                                                        <tr>
+                                                                <th>Name</th>
+                                                                <th>Email</th>
+                                                                <th>Action</th> 
+                                                        </tr>
+                                                </thead>
+                                        </table>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+</div>
+@endsection
+
+@section('script')
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+
+<script type="text/javascript">
+        $(document).ready(function() {
+            $('.datatable').DataTable({
+                processing: true,
+                serverSide: true,
+                pageLength: 5,
+                ajax: '{{ route('getMosque') }}',
+                columns: [
+                    {data: 'name', name: 'name'},
+                    {data: 'email', name: 'email'},
+                    {data: 'Action', name: 'Action', orderable: false, searchable: false}
+                    
+                ]
+            });
+        });
+        </script>
+@endsection
---
+++ resources/views/superadmin/qariah.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+        <div class="row">
+            @component('components.menu')
+            @endcomponent
+        </div>
+</div>
+@endsection
